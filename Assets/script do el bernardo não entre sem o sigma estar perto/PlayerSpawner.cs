7551d60689b38e6b64572f667a950f388af23803
RinaldoSantos99 <rinaldosantos99@gmail.com>
2025-12-03 09:29:30 -0300

    Player Colors

--- Assets/script do el bernardo não entre sem o sigma estar perto/PlayerSpawner.cs
+++ Assets/script do el bernardo não entre sem o sigma estar perto/PlayerSpawner.cs
@@ -18,14 +18,17 @@ public class PlayerSpawner : MonoBehaviour
         {
             playerInput.transform.position = leftSpawn.position;
             p.SetPlayerSide(player.PlayerSide.Player1);
+            var sprite = playerInput.GetComponentInChildren<SpriteRenderer>();
+            sprite.color = new Color(253f / 255f, 202f / 255f, 38f / 255f, 255f / 255f);
             playerInput.transform.localScale = new Vector3(1, 1, 1);
         }
         else if (playerCount == 2)
         {
             playerInput.transform.position = rightSpawn.position;
             p.SetPlayerSide(player.PlayerSide.Player2);
-            playerInput.transform.localScale = new Vector3(-1, 1, 1); // espelha no X
+            var sprite = playerInput.GetComponentInChildren<SpriteRenderer>();
+            sprite.color = new Color(221f / 255f, 50f / 255f, 53f / 255f, 255f / 255f);
+            playerInput.transform.localScale = new Vector3(-1, 1, 1);
         }
     }
-
 }
